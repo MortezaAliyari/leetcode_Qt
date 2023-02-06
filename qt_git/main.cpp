ca4763b0945dc09b2979cf98600dc25116b01a62
morteza <mortezaliyari@gmail.com>
2023-02-06 13:27:26 +0800

    it works!

--- qt_git/main.cpp
+++ qt_git/main.cpp
@@ -2,10 +2,12 @@
 #include<iostream>
 #include<string>
 //hi
+//asdsdfadf
 using namespace std;
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
   cout<<"hello"<<endl;
+  cout<<endl;
   return a.exec();
 }
