e904b280efc3a65fb0759eaad2fef0e9102a3dd8
morteza <mortezaliyari@gmail.com>
2023-02-15 10:43:15 +0800

    int to array func

--- Add to Array-Form of Integer/arrayform/main.cpp
+++ Add to Array-Form of Integer/arrayform/main.cpp
@@ -1,9 +1,33 @@
 #include <QCoreApplication>
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(int argc, char *argv[])
+class Solution {
+public:
+    vector<int> itoarr;
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+
+    }
+    void int_array(int k){
+      if(k==0){
+        return;}
+      else {
+        itoarr.insert(itoarr.begin(),k%10);
+        int_array(k/10);
+      }
+    }
+};
+int main()
 {
-  QCoreApplication a(argc, argv);
-  cout<<"hi"<<endl;
-  return a.exec();
+  int p=18;
+  vector<int> res;
+   Solution s;
+  s.int_array(p);
+  cout<<"befor : "<<s.itoarr.size()<<endl;
+  for (auto it=s.itoarr.begin();it<s.itoarr.end();++it) {
+    cout<<*it<<" ";
+
+  }
+  cout<<endl;
+  return 0;
 }
