41ca764d2441ce6a7b4b040a7a170c31d1e58ada
morteza <mortezaliyari@gmail.com>
2023-03-03 17:26:03 +0800

    a

--- Pointer/smart_pointers/main.cpp
+++ Pointer/smart_pointers/main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<memory>
 using namespace std;
 void printint(int *i){
   cout<<*i<<endl;
@@ -30,9 +30,30 @@ void findminmax(int array[],int size,int* min,int* max){
   }
 
 
-}
-int main(){
 
+}
+class morteza{
+public:
+  morteza(){cout<<"constructor is invoked"<<endl;}
+  ~morteza(){cout<<"Deconstroctur is invoke"<<endl;}
+  void add(){};
+};
+int main(){
+//  morteza m;
+//  unique_ptr<int> ptr=make_unique<int>(25);
+//  shared_ptr<int> ptr1=make_shared<int>(10);
+//  shared_ptr<int> ptr2=ptr1;
+//  cout<<*ptr<<" , "<<*ptr1<<" , "<<*ptr2<<*ptr<<endl;
+{
+    unique_ptr<morteza> ptr=make_unique<morteza>();}{
+  shared_ptr<morteza> ptr1=make_shared<morteza>();
+  shared_ptr<morteza> ptr2=ptr1;
+  cout<<ptr1.get()<<" , "<<ptr2.get()<<endl;}
+  weak_ptr<morteza> ptr4;
+  {
+  shared_ptr<morteza> ptr5=make_shared<morteza>();
+  ptr4=ptr5;}
+//  cout<<*ptr4<<endl;
   int j=1;
   char a='b';
 //  int arr[5]={}
@@ -46,5 +67,6 @@ int main(){
   findminmax(array,5,&min,&max);
   cout<<"min: "<<min<<"max: "<<max<<endl;
   cout<<array;
+  shared_ptr<morteza> ptr6=make_shared<morteza>();
   return 0;
 }
